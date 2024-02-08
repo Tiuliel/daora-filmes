d740cf1feafb46c865e0f42d8c38c11fd5a4cfb9
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-02-08 10:55:26 -0300

    exercicio de estilização

--- App.js
+++ App.js
@@ -1,14 +1,68 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <Text>App</Text>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.viewLogo}>
+          <Text>@TiuLiel Movies 🎞🎥📽🎬📺</Text>
+        </View>
+        <View style={estilos.viewBotoes}>
+          <Button title="Buscar Filmes" />
+          <Button title="Favoritos" />
+        </View>
+        <View style={estilos.viewRodape}>
+          <Button title="Privacidade" />
+          <Button title="Sobre" />
+        </View>
       </SafeAreaView>
     </>
   );
 }
 
-const estilos = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "yellow",
+    flex: 1,
+  },
+
+  viewLogo: {
+    width: "90%",
+    backgroundColor: "green",
+    flex: 0.5,
+    marginLeft: "auto",
+    marginRight: "auto",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+
+  viewBotoes: {
+    width: "90%",
+    backgroundColor: "orange",
+    justifyContent: "space-around",
+    marginLeft: "auto",
+    marginRight: "auto",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    flex: 0.4,
+  },
+
+  viewRodape: {
+    width: "90%",
+    backgroundColor: "red",
+    justifyContent: "space-between",
+    marginLeft: "auto",
+    marginRight: "auto",
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 0.1,
+  },
+});
