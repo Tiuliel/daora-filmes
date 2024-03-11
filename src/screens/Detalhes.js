2318e6c3a10bada6b45e1725fa949c6e4d497ce7
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-03-11 11:24:53 -0300

    exercicio

--- src/screens/Detalhes.js
+++ src/screens/Detalhes.js
@@ -1,12 +1,75 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import SafeContainer from "../components/SafeContainer";
+import imagemAlternativa from "../../assets/images/foto-alternativa.jpg";
+import moment from "moment";
 
-export default function Detalhes() {
+export default function Detalhes({ route }) {
+  const { filme } = route.params;
+
+  const { title, backdrop_path, release_date, vote_average, overview } = filme;
   return (
-    <View>
-      <Text>Detalhes</Text>
-    </View>
+    <SafeContainer>
+      <View style={estilos.subContainer}>
+        <ImageBackground
+          style={estilos.imagemFundo}
+          source={
+            backdrop_path
+              ? { uri: `https://image.tmdb.org/t/p/original/${backdrop_path}` }
+              : imagemAlternativa
+          }
+        >
+          <Text style={estilos.titulo}>{title}</Text>
+        </ImageBackground>
+        <View style={estilos.conteudo}>
+          <ScrollView>
+            <Text style={[estilos.texto, estilos.avaliacao]}>
+              Nota: {vote_average}
+            </Text>
+            <Text style={[estilos.texto, estilos.lancamento]}>
+              Data: {moment(release_date).format("DD/MM/yyyy")}
+            </Text>
+            <Text style={[estilos.texto, estilos.descricao]}>{overview}</Text>
+          </ScrollView>
+        </View>
+      </View>
+    </SafeContainer>
   );
 }
 
-const estilos = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  subContainer: {
+    flex: 1,
+    width: "100%",
+    padding: 8,
+  },
+  imagemFundo: {
+    height: 200,
+    justifyContent: "center",
+  },
+  titulo: {
+    backgroundColor: "rgba(0,0,0,0.75)",
+    color: "white",
+    fontFamily: "NotoSans",
+    textAlign: "center",
+    padding: 16,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  conteudo: {
+    padding: 16,
+    flex: 1,
+  },
+  texto: {
+    paddingVertical: 4,
+    fontSize: 16,
+  },
+  avaliacao: { color: "blue" },
+  lancamento: { color: "darkblue" },
+  descricao: { color: "gray" },
+});
