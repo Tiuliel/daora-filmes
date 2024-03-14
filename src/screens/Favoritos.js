43db4ad6e05006f1162b804378319638cacb1ac1
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-03-14 11:58:23 -0300

    estrutura pag favoritos

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -35,7 +35,7 @@ export default function Favoritos() {
         <View style={estilos.viewFavoritos}>
           <Text style={estilos.texto}>Quantidade: {listaFavoritos.length}</Text>
 
-          <Pressable style={estilos.botao}>
+          <Pressable style={estilos.botaoExcluirFavoritos}>
             <Text style={estilos.textoBotao}>
               <Ionicons name="trash-outline" size={16} /> Excluir favoritos
             </Text>
@@ -43,7 +43,16 @@ export default function Favoritos() {
         </View>
         <ScrollView showsVerticalScrollIndicator={false}>
           {listaFavoritos.map((filme) => {
-            return <Text key={filme.id}>{filme.title}</Text>;
+            return (
+              <View style={estilos.item}>
+                <Pressable style={estilos.botaofilme}>
+                  <Text style={estilos.titulo}>{filme.title}</Text>
+                </Pressable>
+                <Pressable style={estilos.botaoExcluir}>
+                  <Ionicons name="trash" size={16} />
+                </Pressable>
+              </View>
+            );
           })}
         </ScrollView>
       </View>
@@ -57,7 +66,24 @@ const estilos = StyleSheet.create({
     padding: 16,
     width: "100%",
   },
+
+  viewFavoritos: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 8,
+  },
   texto: {
     marginVertical: 8,
+  },
+  botaoExcluirFavoritos: {
+    borderWidth: 1,
+    borderColor: "red",
+    padding: 8,
+    borderRadius: 4,
+  },
+
+  textoBotao: {
+    color: "red",
   },
 });
